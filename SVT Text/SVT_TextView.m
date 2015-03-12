670ff8d7004d5fbd7ba3929152d26625edef127b
Simon Fransson <simon@dessibelle.se>
2015-03-12 19:08:15 +0100

    Initial Commit

---
+++ SVT Text/SVT_TextView.m
@@ -0,0 +1,52 @@
+//
+//  SVT_TextView.m
+//  SVT Text
+//
+//  Created by Simon Fransson on 2015-03-12.
+//  Copyright (c) 2015 Simon Fransson. All rights reserved.
+//
+
+#import "SVT_TextView.h"
+
+@implementation SVT_TextView
+
+- (instancetype)initWithFrame:(NSRect)frame isPreview:(BOOL)isPreview
+{
+    self = [super initWithFrame:frame isPreview:isPreview];
+    if (self) {
+        [self setAnimationTimeInterval:1/30.0];
+    }
+    return self;
+}
+
+- (void)startAnimation
+{
+    [super startAnimation];
+}
+
+- (void)stopAnimation
+{
+    [super stopAnimation];
+}
+
+- (void)drawRect:(NSRect)rect
+{
+    [super drawRect:rect];
+}
+
+- (void)animateOneFrame
+{
+    return;
+}
+
+- (BOOL)hasConfigureSheet
+{
+    return NO;
+}
+
+- (NSWindow*)configureSheet
+{
+    return nil;
+}
+
+@end
